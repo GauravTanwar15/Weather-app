da7f54862b210c53b549a5fc564dbce6c6893906
Gaurav Tanwar <gaurav.tanwar19@bvicam.in>
2021-05-20 20:21:57 +0530

    Api key commit

--- Script.js
+++ Script.js
@@ -1,5 +1,5 @@
 let weather = {
-    apiKey:"Your API Key",
+    apiKey:WEATHER_API_KEY,
     fetchWeather: function (city) {
       fetch(
         "https://api.openweathermap.org/data/2.5/weather?q=" +
